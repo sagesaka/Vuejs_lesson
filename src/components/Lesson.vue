cb84f7c57148346a316e6eb59b62e565d7101742
sagesaka <sagesaka@riprice.co.jp>
2024-09-20 18:09:32 +0900

    リアクティブな状態のオブジェクトを定義する①

--- src/components/Lesson.vue
+++ src/components/Lesson.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 const title = 'Vue.js Lesson'
 let price = ref(9.99)
 
@@ -7,11 +7,19 @@ function increment() {
   price.value += 1
   console.log(price)
 }
+
+// リアクティブな値の宣言はオブジェクトでも可
+const info = ref({
+  students: 1000,
+  rating: 5
+})
+console.log(info.value.students)
 </script>
 
 <template>
   <h1>title: {{ title }}</h1>
   <h2>price: ${{ price }}</h2>
+  <h2>Students: {{ info.students }}</h2>
 
   <button @click="increment">ボタン</button>
 </template>
