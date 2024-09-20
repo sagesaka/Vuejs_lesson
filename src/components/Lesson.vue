b7023a031737e29ef96ddd5f4a7e0bc719e872f0
sagesaka <sagesaka@riprice.co.jp>
2024-09-20 17:44:47 +0900

    ref()関数について理解

--- src/components/Lesson.vue
+++ src/components/Lesson.vue
@@ -1,7 +1,20 @@
 <script setup>
+import { ref } from 'vue'
 const title = 'Vue.js Lesson'
+let price = ref(9.99)
+
+function increment() {
+  price.value += 1
+  console.log(price)
+}
 </script>
 
 <template>
   <h1>title: {{ title }}</h1>
+  <h2>price: ${{ price }}</h2>
+
+  <button @click="increment">ボタン</button>
 </template>
+
+<!--<template>タグ内はrefオブジェクトを認識してくれるので、{{}}内には.valueをつけない
+  <script>タグ内には.valueをつける！-->
