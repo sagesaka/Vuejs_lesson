1a9d38646fccfa8c540056fd83fa1d0db63d9866
sagesaka <sagesaka@riprice.co.jp>
2024-09-24 17:25:24 +0900

    コンフリクト修正

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,7 @@
 <template>
-<<<<<<< Updated upstream
   <Lesson />
 </template>
 
 <script setup>
 import Lesson from './components/Lesson.vue'
-=======
-  <lesson />
-</template>
-
-<script setup>
-import lesson from './lesson.vue'
->>>>>>> Stashed changes
 </script>
